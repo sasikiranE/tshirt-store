90cffb88a2ed86f28f0b5eacc1d4bc23f05458d5
Sasi Kiran E <sasikiranessamaraju@gmail.com>
2022-06-17 19:03:30 +0530

    make sure that jwt secret is set.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 
-const port = process.env.PORT || 3000;
+if (!process.env.JWT_PRIVATE_KEY) {
+  console.log("FATAL ERROR: JWT_PRIVATE_KEY is not set...");
+  process.exit(1);
+}
 
 mongoose
   .connect(process.env.DATABASE)
@@ -19,6 +22,8 @@ mongoose
 app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
